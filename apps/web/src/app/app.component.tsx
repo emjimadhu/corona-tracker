831bc2b76c006b6884d010cfd4635858b8fe8a22
Em Ji Madhu <emjimadhu@gmail.com>
2020-08-13 00:02:18 +0530

    feat(libs-web): :sparkles: add api call to fetch country data

--- apps/web/src/app/app.component.tsx
+++ apps/web/src/app/app.component.tsx
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, {
+  useEffect, useState
+} from 'react';
 import { WebComponentsHeader } from '@corona-tracker/web/components/header';
+import {
+  ISelectButtonCountry, ICountry
+} from '@corona-tracker/shared/types';
 
 import './app.component.scss';
 
 
 export const App = () => {
+  const [
+    countries,
+    setCountries
+  ] = useState<ISelectButtonCountry[]>([]);
+
+  useEffect(() => {
+    const getCountriesList = () => {
+      fetch('https://disease.sh/v3/covid-19/countries')
+        .then(response => response.json())
+        .then((data: ICountry[]) => {
+          const countryList = data.map((country: ICountry) => {
+            return {
+              name: country.country,
+              value: country.countryInfo.iso2
+            };
+          });
+
+          setCountries(countryList);
+        });
+    };
+
+    getCountriesList();
+  }, []);
+
   return (
     <div className="app">
-      <WebComponentsHeader />
+      <WebComponentsHeader
+        countriesList={countries}
+      />
     </div>
   );
 };
--- libs/web/components/header/src/lib/web-components-header.component.tsx
+++ libs/web/components/header/src/lib/web-components-header.component.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   FormControl, Select, MenuItem
 } from '@material-ui/core';
+import { ISelectButtonCountry } from '@corona-tracker/shared/types';
 
 import './web-components-header.component.scss';
 
-export interface IWebComponentsHeaderProps {} // eslint-disable-line @typescript-eslint/no-empty-interface
+export interface IWebComponentsHeaderProps {
+  countriesList: ISelectButtonCountry[];
+}
 
-export const WebComponentsHeader = (properties: IWebComponentsHeaderProps) => {
+export const WebComponentsHeader: React.FC<IWebComponentsHeaderProps> = ({
+  countriesList
+}) => {
   return (
     <div className="header">
       <h1>CORONA TRACKER</h1>
       <FormControl className="header__dropdown">
         <Select
           variant="outlined"
-          value="abc"
+          value=""
         >
-          <MenuItem value="A">A</MenuItem>
-          <MenuItem value="B">B</MenuItem>
-          <MenuItem value="C">C</MenuItem>
-          <MenuItem value="D">D</MenuItem>
+          {
+            countriesList.map((country: ISelectButtonCountry, index: number) => (
+              <MenuItem value={country.value} key={index}>{country.name}</MenuItem>
+            ))
+          }
         </Select>
       </FormControl>
     </div>
