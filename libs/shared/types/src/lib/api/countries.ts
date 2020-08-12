2d612d1f59210fa1ad78ab24b23cd4cef0bc008f
Em Ji Madhu <emjimadhu@gmail.com>
2020-08-12 23:26:53 +0530

    chore(libs-shared): :art: add api response `ICountry` interface

---
+++ libs/shared/types/src/lib/api/countries.ts
@@ -0,0 +1,34 @@
+interface ICountryInfo {
+  _id: number;
+  iso2: string;
+  iso3: string;
+  lat: number;
+  long: number;
+  flag: string;
+}
+
+export interface ICountry {
+  updated: number;
+  country: string;
+  countryInfo: ICountryInfo;
+  cases: number;
+  todayCases: number;
+  deaths: number;
+  todayDeaths: number;
+  recovered: number;
+  todayRecovered: number;
+  active: number;
+  critical: number;
+  casesPerOneMillion: number;
+  deathsPerOneMillion: number;
+  tests: number;
+  testsPerOneMillion: number;
+  population: number;
+  continent: string;
+  oneCasePerPeople: number;
+  oneDeathPerPeople: number;
+  oneTestPerPeople: number;
+  activePerOneMillion: number;
+  recoveredPerOneMillion: number;
+  criticalPerOneMillion: number;
+}
